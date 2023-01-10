50cd78e8047d7b3f7dab871ed2032f367eee3e28
Artem Bilash <artem.bilash@casafari.com>
2023-01-10 16:34:38 +0200

    meta tags all pages

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -3,11 +3,12 @@ from imagekit.models import ProcessedImageField, ImageSpecField
 from pilkit.processors import ResizeToFill
 from django.utils.safestring import mark_safe
 
+from apps.main.mixins import MetaTagMixin
 from apps.user.models import User
 from config.settings import MEDIA_ROOT
 
 
-class BlogCategory(models.Model):
+class BlogCategory(MetaTagMixin):
     name = models.CharField(verbose_name='Имя категории', max_length=255)
     # image = models.ImageField(verbose_name='Изабражение', upload_to='blog/category/', null=True)
     image = ProcessedImageField(
@@ -49,7 +50,7 @@ class Tag(models.Model):
         verbose_name_plural = 'Теги'
 
 
-class Article(models.Model):
+class Article(MetaTagMixin):
     category = models.ForeignKey(to=BlogCategory, verbose_name='Категория', on_delete=models.CASCADE)
     user = models.ForeignKey(User, verbose_name='Автор', on_delete=models.SET_NULL, null=True, blank=True)
     title = models.CharField(verbose_name='Заголовок', max_length=255)
@@ -69,6 +70,11 @@ class Article(models.Model):
     )
     updated_at = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+
+    def get_meta_title(self):
+        if self.meta_title:
+            return self.meta_title
+        return self.title
 
     def __str__(self):
         return self.title
