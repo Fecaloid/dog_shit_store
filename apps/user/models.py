c9139ff6c49d3c91cf75063d7a748401d5cd1448
Artem Bilash <artem.bilash@casafari.com>
2023-01-17 18:10:56 +0200

    register

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,4 +1,3 @@
-from django.db import models
 from  django.contrib.auth.models import AbstractUser
 from django.utils.safestring import mark_safe
 from phonenumber_field.modelfields import PhoneNumberField
